a708dfb62ba619b455a4bce4e44370855e73123f
Osama Sayed <osama.sayed.mohammed@gmail.com>
2020-08-12 10:10:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AnimationApp
+
+A new Flutter project to apply animation concepts, teached on youtube channel named Ninja.
---
+++ lib/shared/screenTitle.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ScreenTitle extends StatelessWidget {
+  final String text;
+
+  const ScreenTitle({Key key, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder(
+      child: Text(
+        text,
+        style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
+      duration: Duration(milliseconds: 999),
+      tween: Tween<double>(begin: 0, end: 1),
+      builder: (BuildContext context,double _val,Widget child){
+        return Opacity(opacity: _val,
+        child: Padding(
+          padding: EdgeInsets.only(top:_val*15),
+          child: child,
+        ),
+        );
+      },
+    );
+  }
+}
